1b4efcafca59fcb2407887f379a39b6cab64d6be
flowrites <flodoesthings@gmail.com>
2020-03-06 20:13:12 +0100

    Fix CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,6 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
-    // 'camelcase': 'off',
     'comma-dangle': 'off',
     'max-len': ['warn', {'code': 150}],
     'no-multiple-empty-lines': ['warn', {'max': 2}],
